b8dedaa162d36d89e720fce46f3ebe9b7cf9962a
pradotiago <pradostiago@gmail.com>
2020-04-23 22:20:14 -0300

    Criando tabela exercício 2

---
+++ ac03_ex2.py
@@ -0,0 +1,12 @@
+import sqlite3
+conn = sqlite3.connect(':memory:')
+c = conn.cursor()
+
+c.execute('''CREATE TABLE exercicio2
+        (id integer primary key,
+        nome text not null,
+        email text)''')
+
+c.execute("INSERT INTO exercicio2 VALUES (1, 'Tiago Prado', 'tiago.prado@aluno.faculdadeimpacta.com.br')")
+
+c.execute("INSERT INTO exercicio2 VALUES (2, 'Bruno dos Santos')")
